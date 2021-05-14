30ee17897fd1941aa5648702237663b332eb3386
Abrar <abrar.nitk@gmail.com>
2021-05-14 17:26:06 +0530

    localhost default for test env

--- realm-client/src/env.rs
+++ realm-client/src/env.rs
@@ -1,4 +1,7 @@
 pub fn host() -> String {
+    if crate::is_test() {
+        return "http://127.0.0.1:3000".to_string();
+    }
     match std::env::var("REALM_HOST") {
         Ok(host) => host,
         Err(_) => "https://www.fifthtry.com".to_string(),
