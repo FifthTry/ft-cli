359524c59e437015ac553a454b56965d019f772c
Abrar <abrar.nitk@gmail.com>
2021-05-22 09:46:59 +0530

    grammar

--- src/id.rs
+++ src/id.rs
@@ -1,6 +1,11 @@
 pub fn to_document_id(path: &str, root_dir: &str, collection_id: &str) -> std::path::PathBuf {
     let path_without_root = std::path::Path::new(path)
         .strip_prefix(root_dir)
-        .unwrap_or_else(|_| panic!("path `{}` is not starts with root_dir `{}`", path, root_dir));
+        .unwrap_or_else(|_| {
+            panic!(
+                "path `{}` does not start with root_dir `{}`",
+                path, root_dir
+            )
+        });
     std::path::PathBuf::from(collection_id).join(&path_without_root)
 }
