f185691e563d7dcb3ebc2746c5aad1f56504712b
Abrar <abrar.nitk@gmail.com>
2021-05-24 09:54:29 +0530

    digest logic

--- ft-api/src/bulk_update.rs
+++ ft-api/src/bulk_update.rs
@@ -68,17 +68,109 @@ pub enum Action {
     Deleted { id: String },
 }
 
-fn digest(actions: &[Action]) -> Vec<Action> {
+impl Action {
+    fn id(&self) -> &str {
+        match self {
+            Self::Updated { id, .. } => id,
+            Self::Added { id, .. } => id,
+            Self::Deleted { id } => id,
+        }
+    }
+
+    fn is_updated(&self) -> bool {
+        matches!(self, Self::Updated { .. })
+    }
+
+    fn is_added(&self) -> bool {
+        matches!(self, Self::Added { .. })
+    }
+
+    fn is_deleted(&self) -> bool {
+        matches!(self, Self::Deleted { .. })
+    }
+}
+
+fn digest(actions: Vec<Action>) -> Vec<Action> {
     // more than one Updated with the same id, ensure each content is exactly same and merge
     // into one
     //
+    let updated: Vec<_> = actions
+        .iter()
+        .filter(|x| x.is_updated())
+        .filter_map(|x| {
+            if let Action::Updated { id, content } = x {
+                Some((id, content))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let updated_map: std::collections::HashMap<_, _> =
+        updated.clone().into_iter().map(|x| x).collect();
+
+    for (id, content) in updated.iter() {
+        if let Some(c) = updated_map.get(id) {
+            if !c.eq(content) {
+                println!("Updated action: content is not same for id: {}", id)
+                // return error
+            }
+        }
+    }
+
+    // TODO: collect unique updated ID's
+
     // more than one Added with the same id, ensure each content is exactly same and merge
     // into one
     //
-    // if both Updated and Added have same id, ensure content matches and than merge Updated
-    //
+
+    let added: Vec<_> = actions
+        .iter()
+        .filter(|x| x.is_added())
+        .filter_map(|x| {
+            if let Action::Added { id, content } = x {
+                Some((id, content))
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let added_map: std::collections::HashMap<_, _> = added.clone().into_iter().map(|x| x).collect();
+
+    for (id, content) in added.iter() {
+        if let Some(c) = added_map.get(id) {
+            if !c.eq(content) {
+                println!("Added action: content is not same for id: {}", id)
+                // return error
+            }
+        }
+    }
+    // TODO: collect unique added ID's
+
     // if more than one Deleted with the same id, merge into one
     //
+
+    let deleted: Vec<_> = actions
+        .iter()
+        .filter(|x| x.is_deleted())
+        .filter_map(|x| {
+            if let Action::Deleted { id } = x {
+                Some(id)
+            } else {
+                None
+            }
+        })
+        .collect();
+
+    let deleted: std::collections::HashMap<_, _> =
+        deleted.clone().into_iter().map(|x| (x, true)).collect();
+
+    // TODO: collect unique deleted ID's
+
+    // if both Updated and Added have same id, ensure content matches and than merge Updated
+    //
+
     // if something is added and deleted, return Error::AddedAndDeleted {id: ""}
     //
     // if content is both Updated and Deleted, only send Deleted
