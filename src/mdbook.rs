4cf612f69dfe8b8b4e0fa66ec62a8679fdfe3d2c
Abrar <abrar.nitk@gmail.com>
2021-05-25 16:55:56 +0530

    handle mdbook files

--- src/mdbook.rs
+++ src/mdbook.rs
@@ -3,13 +3,62 @@ pub fn handle_files(
     files: &[crate::FileMode],
 ) -> crate::Result<Vec<ft_api::bulk_update::Action>> {
     let mdbook_config = mdbook::config::Config::default();
+
     let book = mdbook::MDBook::init(config.root.as_str())
         .create_gitignore(true)
         .with_config(mdbook_config)
         .build()
         .expect("Book generation failed");
 
-    println!("{:#?}", book.book);
+    let mut actions = vec![];
+    for file in files.iter() {
+        actions.append(&mut self::handle(
+            &file,
+            config.root.as_str(),
+            config.collection.as_str(),
+        )?);
+    }
 
-    Ok(vec![])
+    if files.iter().any(|v| {
+        matches!(v, crate::FileMode::Created(_)) || matches!(v, crate::FileMode::Deleted(_))
+    }) {
+        // actions.push(self::index(&tree, config)?)
+    }
+
+    println!("actions: {:#?}", actions);
+    // for x in book.book.iter() {
+    //     println!("{:#?}", x);
+    // }
+
+    Ok(actions)
+}
+
+fn handle(
+    file: &crate::FileMode,
+    root: &str,
+    collection: &str,
+) -> crate::Result<Vec<ft_api::bulk_update::Action>> {
+    if file.extension() != "md" {
+        return Ok(vec![]);
+    }
+    let id = file.id_with_extension(root, collection);
+    Ok(match file {
+        crate::types::FileMode::Created(_) => {
+            println!("Created: {}", id.as_str());
+            vec![ft_api::bulk_update::Action::Added {
+                content: file.raw_content(&id)?,
+                id,
+            }]
+        }
+        crate::types::FileMode::Modified(_) => {
+            println!("Updated: {}", id.as_str());
+            vec![ft_api::bulk_update::Action::Updated {
+                content: file.raw_content(&id)?,
+                id,
+            }]
+        }
+        crate::types::FileMode::Deleted(_) => {
+            vec![ft_api::bulk_update::Action::Deleted { id }]
+        }
+    })
 }
