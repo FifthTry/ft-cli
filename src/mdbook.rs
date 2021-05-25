2c0cad3097e22ad4a194b7b16173c30a4373a5bc
Abrar <abrar.nitk@gmail.com>
2021-05-25 18:06:04 +0530

    build toc

--- src/mdbook.rs
+++ src/mdbook.rs
@@ -10,6 +10,7 @@ pub fn handle_files(
         .build()
         .expect("Book generation failed");
 
+    // TODO: Need to discuss it with amitu about appending "src"
     let root_path = std::path::Path::new(config.root.as_str()).join("src");
     let mut actions = vec![];
     for file in files.iter() {
@@ -20,17 +21,14 @@ pub fn handle_files(
         )?);
     }
 
+    self::index(&book.book, config)?;
     if files.iter().any(|v| {
         matches!(v, crate::FileMode::Created(_)) || matches!(v, crate::FileMode::Deleted(_))
     }) {
-        // actions.push(self::index(&tree, config)?)
+        actions.push(self::index(&book.book, config)?)
     }
 
     println!("actions: {:#?}", actions);
-    // for x in book.book.iter() {
-    //     println!("{:#?}", x);
-    // }
-
     Ok(actions)
 }
 
@@ -39,6 +37,7 @@ fn handle(
     root: &str,
     collection: &str,
 ) -> crate::Result<Vec<ft_api::bulk_update::Action>> {
+    // TODO: Need to discuss with amitu
     if file.extension() != "md" {
         return Ok(vec![]);
     }
@@ -63,4 +62,59 @@ fn handle(
             vec![ft_api::bulk_update::Action::Deleted { id }]
         }
     })
+}
+
+fn index(
+    book: &mdbook::book::Book,
+    config: &crate::Config,
+) -> crate::Result<ft_api::bulk_update::Action> {
+    let mut sections = vec![ftd::Section::ToC(self::to_ftd_toc(
+        book,
+        config.collection.as_str(),
+    ))];
+    sections.extend_from_slice(&config.index_extra);
+
+    println!("Updated: {}", config.collection.as_str());
+    Ok(ft_api::bulk_update::Action::Updated {
+        id: config.collection.to_string(),
+        content: ftd::Document::new(&sections).convert_to_string(),
+    })
+}
+
+fn to_ftd_toc(book: &mdbook::book::Book, collection_id: &str) -> ftd::toc::ToC {
+    fn path_to_doc_id(path: &str, collection_id: &str) -> std::path::PathBuf {
+        std::path::PathBuf::from(collection_id).join(path)
+    }
+
+    fn to_ftd_items(items: &[mdbook::BookItem], collection_id: &str) -> Vec<ftd::toc::TocItem> {
+        let mut toc_items = vec![];
+        for item in items.iter() {
+            match item {
+                mdbook::BookItem::Chapter(chapter) => {
+                    // TODO: chapter.source_path, chapter.path both are optional
+                    let id = path_to_doc_id(
+                        &chapter.path.as_ref().unwrap().to_string_lossy(),
+                        collection_id,
+                    )
+                    .to_string_lossy()
+                    .to_string();
+                    // println!(
+                    //     "Title: {}, Id: {:?}, {:?}",
+                    //     chapter, chapter.source_path, &id
+                    // );
+                    let mut item = ftd::toc::TocItem::with_title_and_id(&chapter.to_string(), &id);
+                    item.children = to_ftd_items(&chapter.sub_items, collection_id);
+                    toc_items.push(item);
+                }
+                _ => {
+                    // TODO: Need to discuss what to do with other sections
+                }
+            }
+        }
+        toc_items
+    }
+
+    ftd::ToC {
+        items: to_ftd_items(&book.sections, collection_id),
+    }
 }
