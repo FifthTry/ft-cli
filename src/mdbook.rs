d42ba28770801d44b0e7a8b40244e3d4f07f0809
Abrar <abrar.nitk@gmail.com>
2021-05-25 13:20:28 +0530

    added mdbook init

--- src/mdbook.rs
+++ src/mdbook.rs
@@ -2,5 +2,14 @@ pub fn handle_files(
     config: &crate::Config,
     files: &[crate::FileMode],
 ) -> crate::Result<Vec<ft_api::bulk_update::Action>> {
+    let mdbook_config = mdbook::config::Config::default();
+    let book = mdbook::MDBook::init(config.root.as_str())
+        .create_gitignore(true)
+        .with_config(mdbook_config)
+        .build()
+        .expect("Book generation failed");
+
+    println!("{:#?}", book.book);
+
     Ok(vec![])
 }
