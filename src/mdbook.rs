0d723568cdc552dea49e44dd7da071ef02998228
Abrar <abrar.nitk@gmail.com>
2021-05-26 15:36:02 +0530

    summary parse and find the title

--- src/mdbook.rs
+++ src/mdbook.rs
@@ -16,14 +16,20 @@ pub fn handle_files(
             mdbook::MDBook::load_with_config(book_root, config).expect(""),
         )
     };
+
+    let src_dir = std::path::Path::new(config.root.as_str()).join(&book_config.book.src);
+
+    let summary = self::summary_content(&src_dir).unwrap();
+
+    // println!("{:#?}", summary);
     // println!("{:#?}", book.book);
 
-    let root_path = std::path::Path::new(config.root.as_str()).join(&book_config.book.src);
     let mut actions = vec![];
     for file in files.iter() {
         actions.append(&mut self::handle(
+            &summary,
             &file,
-            &root_path.to_string_lossy(),
+            &src_dir.to_string_lossy(),
             config.collection.as_str(),
         )?);
     }
@@ -31,11 +37,12 @@ pub fn handle_files(
     // TODO: Need to remove it from this place
     actions.push(self::index(&book.book, config, &book_config.book.src)?);
 
-    println!("actions: {:#?}", actions);
+    // println!("actions: {:#?}", actions);
     Ok(actions)
 }
 
 fn handle(
+    summary: &mdbook::book::Summary,
     file: &crate::FileMode,
     root: &str,
     collection: &str,
@@ -52,17 +59,21 @@ fn handle(
 
     let id = file.id_with_extension(root, collection);
     Ok(match file {
-        crate::types::FileMode::Created(_) => {
+        crate::types::FileMode::Created(file_path) => {
             println!("Created: {}", id.as_str());
+            let title = self::chapter_title(&summary, &std::path::Path::new(file_path))
+                .unwrap_or_else(|| id.clone());
             vec![ft_api::bulk_update::Action::Added {
-                content: file.raw_content(&id)?,
+                content: file.raw_content(&title)?,
                 id,
             }]
         }
-        crate::types::FileMode::Modified(_) => {
+        crate::types::FileMode::Modified(file_path) => {
             println!("Updated: {}", id.as_str());
+            let title = self::chapter_title(&summary, &std::path::Path::new(file_path))
+                .unwrap_or_else(|| id.clone());
             vec![ft_api::bulk_update::Action::Updated {
-                content: file.raw_content(&id)?,
+                content: file.raw_content(&title)?,
                 id,
             }]
         }
@@ -150,4 +161,54 @@ fn to_ftd_toc(book: &mdbook::book::Book, collection_id: &str) -> ftd::toc::ToC {
     ftd::ToC {
         items: to_ftd_items(&book.sections, collection_id),
     }
+}
+
+pub fn summary_content(src_dir: &std::path::Path) -> mdbook::errors::Result<mdbook::book::Summary> {
+    use std::io::Read;
+    let summary_md = src_dir.join("SUMMARY.md");
+
+    let mut summary_content = String::new();
+    std::fs::File::open(&summary_md)?.read_to_string(&mut summary_content)?;
+
+    let summary = mdbook::book::parse_summary(&summary_content)?;
+
+    // if create_missing {
+    //     create_missing(&src_dir, &summary).with_context(|| "Unable to create missing chapters")?;
+    // }
+
+    Ok(summary)
+}
+
+fn chapter_title(summary: &mdbook::book::Summary, file_name: &std::path::Path) -> Option<String> {
+    fn find_in_items(
+        items: &[mdbook::book::SummaryItem],
+        file_name: &std::path::Path,
+    ) -> Option<String> {
+        for item in items {
+            match match item {
+                mdbook::book::SummaryItem::Link(link) => {
+                    if let Some(name) = link.location.as_ref() {
+                        if name.eq(file_name) {
+                            return Some(link.name.to_string());
+                        }
+                    }
+                    find_in_items(&link.nested_items, file_name)
+                }
+                mdbook::book::SummaryItem::PartTitle(_) => None,
+                mdbook::book::SummaryItem::Separator => None,
+            } {
+                Some(s) => return Some(s),
+                None => continue,
+            };
+        }
+        None
+    }
+
+    find_in_items(&summary.numbered_chapters, file_name)
+        .or_else(|| find_in_items(&summary.prefix_chapters, file_name))
+        .or_else(|| find_in_items(&summary.suffix_chapters, file_name))
+}
+
+fn summary_title(summary: &mdbook::book::Summary) -> Option<String> {
+    summary.title.clone()
 }
