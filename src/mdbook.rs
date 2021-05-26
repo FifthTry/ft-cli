4e0533668223fda7f0c327784a101a384671d71f
Abrar <abrar.nitk@gmail.com>
2021-05-26 17:19:55 +0530

    added log lib for logging purpose

--- src/mdbook.rs
+++ src/mdbook.rs
@@ -2,10 +2,23 @@ pub fn handle_files(
     config: &crate::Config,
     files: &[crate::FileMode],
 ) -> crate::Result<Vec<ft_api::bulk_update::Action>> {
+    use log::warn;
+
     let (book_config, book) = {
         let book_root = config.root.as_str();
         let book_root: std::path::PathBuf = book_root.into();
         let config_location = book_root.join("book.toml");
+
+        // the book.json file is no longer used, so we should emit a warning to
+        // let people know to migrate to book.toml
+        if book_root.join("book.json").exists() {
+            warn!("It appears you are still using book.json for configuration.");
+            warn!("This format is no longer used, so you should migrate to the");
+            warn!("book.toml format.");
+            warn!("Check the user guide for migration information:");
+            warn!("\thttps://rust-lang.github.io/mdBook/format/config.html");
+        }
+
         let config = if config_location.exists() {
             mdbook::config::Config::from_disk(&config_location).expect("book.toml does not exists")
         } else {
