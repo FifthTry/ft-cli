8b5df8488e3ee5a189d2f740bb090e4955249d51
Abrar <abrar.nitk@gmail.com>
2021-05-31 15:20:44 +0530

    parse backtick/fenceblock

--- src/mdbook.rs
+++ src/mdbook.rs
@@ -360,3 +360,57 @@ fn content_with_extract_title(content: &str) -> (String, Option<String>) {
         }),
     )
 }
+
+fn replace_backtick(content: &str) -> String {
+    #[derive(PartialEq)]
+    enum ParsingState {
+        WaitingForBackTick,
+        WaitingForEndBackTick,
+    }
+
+    struct State {
+        state: ParsingState,
+        sections: Vec<String>,
+    }
+
+    let mut state = State {
+        state: ParsingState::WaitingForBackTick,
+        sections: vec![],
+    };
+
+    fn parse_lang(line: &str) -> String {
+        let line = line.replace("```", "").split(",").collect::<Vec<_>>();
+        "txt".to_string()
+    }
+
+    fn finalize(state: State) -> String {
+        state.sections.join("\n")
+    }
+
+    let mut buffer: String = "".to_string();
+    for line in content.split("\n") {
+        if line.trim().starts_with("```") && state.state == ParsingState::WaitingForBackTick {
+            let lang = parse_lang(line);
+            if !buffer.trim().eq("-- markdown:") {
+                state.sections.push(buffer.drain(..).collect());
+            } else {
+                buffer.drain(..);
+            }
+
+            state.state = ParsingState::WaitingForEndBackTick;
+            buffer = format!("-- code:\nlang: {}\n", lang);
+        } else if line.trim().starts_with("```")
+            && state.state == ParsingState::WaitingForEndBackTick
+        {
+            state.sections.push(buffer.drain(..).collect());
+            state.state = ParsingState::WaitingForBackTick;
+            buffer = "-- markdown:\n\n".to_string();
+        } else {
+            buffer.push_str(line);
+            buffer.push_str("\n");
+        }
+    }
+
+    state.sections.push(buffer.drain(..).collect());
+    finalize(state)
+}
